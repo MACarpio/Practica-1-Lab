496c49fecc8bf8b2baaff11a9f88ff3b0ea68504
Mauricio Apaza <mpacarpio@gmail.com>
2021-08-28 21:09:30 -0500

    Creacion del proyecto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Practica_1_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
